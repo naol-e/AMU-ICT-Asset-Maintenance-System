b702beaf14c6b0f7ea8b3779c7af57093d78b128
Ahmeddin <ahmedinman16@gmail.com>
2026-01-23 23:04:35 +0300

    Added Manager functions

---
+++ src/ICTManager.java
@@ -0,0 +1,11 @@
+public class ICTManager extends User {
+    private String managerID;
+
+    public void assignMaintenanceTask(String technicianID, String reportID) {
+        // Logic to assign a task
+    }
+
+    public void generateReports(String reportType) {
+        // Logic to generate PDF report
+    }
+}
