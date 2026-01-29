2159a5d0843c9c15f16eec9224d44d9907295803
Eyob Weldetensay <eyobwldetensay@gmail.com>
2026-01-29 11:34:29 +0300

    LabTechnician_Class

---
+++ src/LabTechnician.java
@@ -0,0 +1,15 @@
+public class LabTechnician extends User {
+    private String technicianID;
+
+    public void registerAsset(Asset asset) {
+        // Logic to add new asset to database
+    }
+
+    public void updateAssetStatus(String assetID, String status) {
+        // Logic to update status
+    }
+
+    public void performMaintenance(String taskID) {
+        // Logic to log maintenance work
+    }
+}
